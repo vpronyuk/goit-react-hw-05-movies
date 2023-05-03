27e96264ba625328881c3c63741ed8a06bdb8c4e
Volodymyr Proniuk <111579555+vpronyuk@users.noreply.github.com>
2023-05-03 22:17:05 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { lazy } from 'react';
 import SharedLayout from './layout/Layout';
 
-const Home = lazy(() => import('pages/home/Home'));
-const Movies = lazy(() => import('pages/movies/Movies'));
-const MovieDetails = lazy(() => import('pages/movieDetails/MovieDetails'));
+const Home = lazy(() => import('../pages/home/Home'));
+const Movies = lazy(() => import('../pages/movies/Movies'));
+const MovieDetails = lazy(() => import('../pages/movieDetails/MovieDetails'));
 
-const Cast = lazy(() => import('components/cast/Cast'));
-const Reviews = lazy(() => import('components/reviews/Reviews'));
+const Cast = lazy(() => import('./cast/Cast'));
+const Reviews = lazy(() => import('./reviews/Reviews'));
 
 export const App = () => {
   return (
